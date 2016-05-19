b4fe4c0ed01bbadd9cde14848f446e07ed16c7a0
Ryan B <ryan.bliton@gmail.com>
2016-05-18 21:20:07 -0500

    angular

--- app/app.js
+++ app/app.js
@@ -1,1 +1,9 @@
-console.log("hello");
+var app = angular.module("TodoApp", []);
+
+app.controller("NavCtrl", function($scope){
+  $scope.navItems = [{name:"Logout"}, {name: "All Items"}, {name:"New Items"}]
+})
+
+app. controller("TodoCtrl", function($scope){
+  $scope.welcome = "hello"
+})
