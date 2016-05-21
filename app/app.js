f7503ce5fffc7cf3b73d7bd7e8f4dea3cd868599
Ryan B <ryan.bliton@gmail.com>
2016-05-21 09:52:34 -0500

    todo buttons

--- app/app.js
+++ app/app.js
@@ -6,4 +6,10 @@ app.controller("NavCtrl", function($scope){
 
 app. controller("TodoCtrl", function($scope){
   $scope.welcome = "hello"
+  $scope.newItem = function(){
+    console.log("you clicked new item");
+  };
+  $scope.allItem = function(){
+    console.log("all item");
+  };
 })
