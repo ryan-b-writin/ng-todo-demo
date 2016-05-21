0edb8df0dfc8585c4c476ad89f633903bef09f64
Ryan B <ryan.bliton@gmail.com>
2016-05-21 10:12:57 -0500

    basic click functions

--- app/app.js
+++ app/app.js
@@ -4,12 +4,16 @@ app.controller("NavCtrl", function($scope){
   $scope.navItems = [{name:"Logout"}, {name: "All Items"}, {name:"New Items"}]
 })
 
-app. controller("TodoCtrl", function($scope){
+app.controller("TodoCtrl", function($scope){
   $scope.welcome = "hello"
+  $scope.showListView = true;
+
   $scope.newItem = function(){
     console.log("you clicked new item");
+    $scope.showListView = false;
   };
   $scope.allItem = function(){
     console.log("all item");
+    $scope.showListView = true;
   };
 })
