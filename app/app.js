08af073b5fac798239a376dbb1f601e3e6e24540
Ryan B <ryan.bliton@gmail.com>
2016-05-21 10:44:50 -0500

    forms

--- app/app.js
+++ app/app.js
@@ -6,7 +6,41 @@ app.controller("NavCtrl", function($scope){
 
 app.controller("TodoCtrl", function($scope){
   $scope.welcome = "hello"
-  $scope.showListView = true;
+  $scope.showListView = false;
+  $scope.newTask = {};
+  
+  $scope.items = [
+    {
+      id: 0,
+      task: "mow the lawn",
+      isCompleted: true,
+      dueDate: "12/5/17",
+      assignedTo: "greg",
+      location: "Zoe's house",
+      urgency: "low",
+      dependencies: ["sunshine", "clippers", "hat", "water", "headphones"]
+    },
+    {
+      id: 1,
+      task: "grade quizzes",
+      isCompleted: false,
+      dueDate: "12/5/15",
+      assignedTo: "joe",
+      location: "NSS",
+      urgency: "high",
+      dependencies: ["sunshine", "clippers", "hat", "water", "headphones"]
+    },
+    {
+      id: 2,
+      task: "nap",
+      isCompleted: true,
+      dueDate: "5/21/16",
+      assignedTo: "zoe",
+      location: "Zoe's house",
+      urgency: "low",
+      dependencies: ["sunshine", "clippers", "hat", "water", "headphones"]
+    }
+  ]
 
   $scope.newItem = function(){
     console.log("you clicked new item");
