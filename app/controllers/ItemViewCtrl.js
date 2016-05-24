bb70052eedafe04996277808564fe7555b1420de
Ryan B <ryan.bliton@gmail.com>
2016-05-23 20:52:38 -0500

    view item details page

--- app/controllers/ItemViewCtrl.js
+++ app/controllers/ItemViewCtrl.js
@@ -1,4 +1,18 @@
-app.controller("ItemViewCtrl", function($scope){
-  $scope.welcome = "hello"
+app.controller("ItemViewCtrl", function($scope, $http, $routeParams){
+  $scope.selectedItem = {};
+  $scope.items = [];
+  $http.get("https://to-do-app-hello.firebaseio.com/items.json")
+    .success(function(itemObject){
+      var itemCollection = itemObject;
+      Object.keys(itemCollection).forEach(function(key){
+        itemCollection[key].id = key;
+        $scope.items.push(itemCollection[key]);
+
+        $scope.selectedItem = $scope.items.filter(function(item){
+          return item.id === $routeParams.itemId;
+        })[0];
+      })
+      
+    })
 
 })
